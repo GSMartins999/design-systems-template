7590a457a360c20c13165779f86eca7645790336
Giovanni Souza <GSMartins999>
2023-10-22 12:34:15 -0300

    chakra

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import CardTwo from "../src/components/CardTwo.js"
+import {theme} from "../src/theme.js"
 
 export default function App() {
   const [users, setUsers] = useState([]);
@@ -22,9 +25,14 @@ export default function App() {
   };
 
   return (
-    <>
-      <h1>Me apague quando for iniciar!</h1>
-      <p>Chame o Card aqui!</p>
-    </>
+    <ChakraProvider theme={theme}>
+      {/* <h1>Me apague quando for iniciar!</h1>
+      <p>Chame o Card aqui!</p> */}
+      {
+        users.map((user) => {
+          return <CardTwo key={user.name} name={user.name} company={user.company} email={user.email} website={user.website}/>
+        })
+      }
+    </ChakraProvider>
   );
 }
